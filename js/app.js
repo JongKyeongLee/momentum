f6970b2ea3cc0b2bddf72824d028661ebf035e3f
JongKyeongLee <111289641+JongKyeongLee@users.noreply.github.com>
2023-01-04 21:00:20 +0900

    momentum-form

--- js/app.js
+++ js/app.js
@@ -1,21 +1,15 @@
-const guessForm = document.querySelector("#guess");
-const max = document.querySelector("#guess h3 input");
-const chose = document.querySelector("#guess h4 input");
-const message1 = document.querySelector(".message span:first-child");
-const message2 = document.querySelector(".message span:last-child");
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
 
-function guessPlay(event) {
+const HIDDEN_CLASSNAME = "hidden";
+
+function onLoginSubmit(event) {
   event.preventDefault();
-  const maxNumber = max.value;
-  const choseNumber = chose.value;
-
-  const machine = Math.ceil(Math.random() * maxNumber);
-  message1.innerText = `You chose: ${choseNumber} the machine chose: ${machine}.`;
-  if (parseInt(choseNumber) === parseInt(machine)) {
-    message2.innerText = "You Won!";
-  } else {
-    message2.innerText = "You Lost!";
-  }
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  const username = loginInput.value;
+  greeting.innerText = `Hello ${username}`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
-guessForm.addEventListener("submit", guessPlay);
+loginForm.addEventListener("submit", onLoginSubmit);
